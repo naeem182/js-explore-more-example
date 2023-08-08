e5614f9c5dfa10132c23f66d15a303e53ea93b42
naeem <naeemislamjr182@gmail.com>
2023-08-08 21:38:47 +0600

    check divisable and tthen print

--- divisable_print.js
+++ divisable_print.js
@@ -1,3 +1,9 @@
+/* 
+1. show output from: 1-50
+2. if the number is divisible by 3 then instead of the number, show 'foo'
+3. if the number is divisible by 5 then instead of the number, show 'bar'
+4. if the number is divisible by both 3 and 5 then instead of the number show 'foobar'
+*/
 for (let i = 0; i < 50; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('foobar');
